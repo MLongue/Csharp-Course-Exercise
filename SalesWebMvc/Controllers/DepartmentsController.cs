d1135eed9eeafae56f89d74e65e9cd2b98738a5e
Murilo Denech Longue <murilo.longue@ska.com.br>
2021-06-08 15:15:19 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Controllers/DepartmentsController.cs
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -14,6 +14,7 @@ namespace SalesWebMvc.Controllers
             List<Department> list = new List<Department>();
             list.Add(new Department { Id = 1, Name = "Eletronics" });
             list.Add(new Department { Id = 2, Name = "Fashion" });
+            list.Add(new Department { Id = 2, Name = "Tools" });
 
             return View(list);
         }
